1954d574bd670e6419cf2ed4e285502c610cf397
Seongwook Kim <bluelove8939@naver.com>
2022-06-04 18:03:35 +0900

    Initial commit

--- compression.c
+++ compression.c
@@ -9,34 +9,9 @@ CompressionResult base_delta_immediate(CacheLine original) {
 }
 
 CompressionResult bdi_compressing_unit(CacheLine original, int k, int d) {
-    void *splitted;
-
-    switch (k) {
-        case 8:
-            splitted = (signed long int *)original;
-            break;
-        
-        case 4:
-            splitted = (signed int *)original;
-            break;
-
-        case 2:
-            splitted = (signed short *)original;
-            break;
-        
-        default:
-            break;
-    }
-        
-
+    
 }
 
 int main(int argc, char const *argv[]) {
-    CacheLine original = (CacheLine)malloc(CACHE64SIZ);
-    memset(original, 0, CACHE64SIZ);
-    original[1] = 1;
-    printf("size: %d\n", sizeof(original));
-    printf("size: %d\n", CACHE64SIZ);
-
     return 0;
 }
--- compression.h
+++ compression.h
@@ -13,13 +13,20 @@
 #define CACHE32SIZ   32
 #define CACHE64SIZ   64
 #define CACHE128SIZ  128
-typedef int* CacheLine;
-typedef int* MetaData;
 
 // Parameters for boolean expression
 #define FALSE 0
 #define TRUE  1
 typedef int  Bool;
+
+// Structures for representing memory chunks(blocks)
+typedef struct {
+    int size;
+    int *body;
+} MemoryChunk;
+
+typedef MemoryChunk CacheLine;
+typedef MemoryChunk MetaData;
 
 typedef struct {
     CacheLine compressed;
@@ -28,9 +35,27 @@ typedef struct {
     int tag_overhead_siz;
 } CompressionResult;
 
-// BDI algorithms related functions
-CompressionResult base_plus_delta(CacheLine original);       // Base + delta algotithm 
-CompressionResult base_delta_immediate(CacheLine original);  // BDI algorithm
+// Functions for managing memory chunks
+MemoryChunk make_memory_chunk(int size, int initial) {
+    MemoryChunk result;
+    result.size = size;
+    result.body = (int *)malloc(size);
+    return result;
+}
+
+void remove_memory_chunk(MemoryChunk chunk) {
+    free(chunk.body);
+}
+
+void print_memory_chunk(MemoryChunk chunk) {
+    for (int i = 0; i < size / sizeof(*chunk); i++) {
+        printf('0x%02x ', chunk.body[i]);
+    }
+}
+
+// Functions for BDI algorithms related functions
+CompressionResult base_plus_delta(CacheLine original);                     // Base + delta algotithm 
+CompressionResult base_delta_immediate(CacheLine original);                // BDI algorithm
 CompressionResult bdi_compressing_unit(CacheLine original, int k, int d);  // Compressing Unit (CU)
 
 #endif

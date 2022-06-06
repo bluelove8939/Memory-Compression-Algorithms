9dc06febe8def3b4acb0b12353e62adc96d8f4c1
Seongwook Kim <bluelove8939@naver.com>
2022-06-06 10:31:00 +0900

    Update BDI compressiong units

--- compression.c
+++ compression.c
@@ -1,17 +1,143 @@
 #include "compression.h"
 
-CompressionResult base_plus_delta(CacheLine original) {
+CacheLine base_plus_delta(CacheLine original) {
     CompressionResult result;
 }
 
-CompressionResult base_delta_immediate(CacheLine original) {
+CacheLine base_delta_immediate(CacheLine original) {
+    CompressionResult result;
+    CacheLine compressed;
 
+    for (int encoding = 0; encoding < 8; encoding++) {
+        compressed = bdi_compressing_unit(original, encoding);
+        if (compressed.size < original.size) {
+            result.compressed = compressed;
+            result.is_compressed = TRUE;
+        }
+    }
 }
 
-CompressionResult bdi_compressing_unit(CacheLine original, int k, int d) {
+CacheLine bdi_compressing_unit(CacheLine original, int encoding) {
+    ValueBuffer base = 0;
+    ValueBuffer buffer, delta, mask;
+    int k, d;
+    int compressed_siz = 0;
+    Bool initial, flag = TRUE;
+    CacheLine result;
+
+    // 0. Select mode by given encoding (MUX)
+    switch (encoding) {
+    case 0:  // Zero values
+        for (int i = 0; i < original.size; i++) {
+            if (original.body[i] != 0) {
+                result = copy_memory_chunk(original);
+                return result;
+            }
+        }
+        result = make_memory_chunk(1, 0);
+        return result;
     
+    case 1:  // Repeating values
+        base = get_value(original.body, 0, 8);
+        for (int i = 0; i < original.size; i += 8) {
+            if (get_value(original.body, i, 8) != base) {
+                result = copy_memory_chunk(original);
+                return result;
+            }
+        }
+        result = make_memory_chunk(8, 0);
+        set_value(result.body, base, 0, 8);
+        return result;
+
+    case 2:  // Base8-delta1
+        k = 8; 
+        d = 1;
+        break;
+    
+    case 3:  // Base8-delta2
+        k = 8; 
+        d = 2;
+        break;
+
+    case 4:  // Base8-delta4
+        k = 8; 
+        d = 4;
+        break;
+
+    case 5:  // Base4-delta1
+        k = 4; 
+        d = 1;
+        break;
+
+    case 6:  // Base4-delta2
+        k = 4; 
+        d = 2;
+        break;
+    
+    case 7:  // Base2-delta1
+        k = 2; 
+        d = 1;
+        break;
+    
+    default:
+        k = 8; 
+        d = 1;
+        break;
+    }
+
+    result = make_memory_chunk(original.size, 0);
+
+    // 1. Find out base value
+    base = get_value(original.body, 0, k);
+    set_value(result.body, base, 0, k);
+    compressed_siz = k;
+
+    // 2. Calculate delta values and check if this cache line is compressible
+    for (int i = 0; i < original.size; i += k) {
+        // 2-1. Split byte array into k-byte array and extract each value
+        buffer = get_value(original.body, i, k);
+        delta = buffer - base;
+        printf("[ITER %2d] base: %08x  buffer: %08x  delta: %08x\n", i/k, base, buffer, delta);
+
+        // 2-2. Check whether calculated delta value is sign-extended within d-Bytes
+        mask = 1;
+        initial = (delta & mask << (sizeof(delta) * BYTE_BITWIDTH - 1)) != 0;
+        
+        for (int j = sizeof(delta) * 8 - 1; j >= d * 8 - 1; j--) {
+            if (initial != ((delta & mask << j) != 0)) {
+                flag = FALSE;
+                break;
+            }
+        }
+
+        // 2-3 Copy shrinked delta value to compressed memory block
+        if (flag == FALSE) {
+            printf("iteration terminated (not compressible)\n");
+            remove_memory_chunk(result);
+            result = copy_memory_chunk(original);
+            return result;
+        } else {
+            mask = 1;
+            set_value(result.body, delta, compressed_siz, d);
+            compressed_siz += d;
+        }
+    }
+
+    result.size = compressed_siz;
+    return result;
 }
 
 int main(int argc, char const *argv[]) {
+    CacheLine original = make_memory_chunk(CACHE64SIZ, 0);
+    *(original.body+1) = 36;
+    print_memory_chunk(original);
+    
+    int encoding = 3;
+    CacheLine compressed = bdi_compressing_unit(original, encoding);
+    print_memory_chunk(compressed);
+
+    remove_memory_chunk(compressed);
+    remove_memory_chunk(original);
+
     return 0;
 }
--- compression.h
+++ compression.h
@@ -4,25 +4,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
+#include <math.h>
 
 // Parameters for cache line size
-#define BYTESIZ      1
-#define HWORDSIZ     2
-#define WORDSIZ      4
-#define DWORDSIZ     8
-#define CACHE32SIZ   32
-#define CACHE64SIZ   64
-#define CACHE128SIZ  128
+#define BYTE_BITWIDTH  8
+#define BYTESIZ        1
+#define HWORDSIZ       2
+#define WORDSIZ        4
+#define DWORDSIZ       8
+#define CACHE32SIZ     32
+#define CACHE64SIZ     64
+#define CACHE128SIZ    128
 
 // Parameters for boolean expression
 #define FALSE 0
 #define TRUE  1
-typedef int  Bool;
+typedef char  Bool;
 
 // Structures for representing memory chunks(blocks)
+typedef char     Byte;
+typedef char *   ByteArr;
+typedef long int ValueBuffer;
+
 typedef struct {
     int size;
-    int *body;
+    int valid_bitwidth;
+    char *body;
 } MemoryChunk;
 
 typedef MemoryChunk CacheLine;
@@ -35,27 +42,76 @@ typedef struct {
     int tag_overhead_siz;
 } CompressionResult;
 
-// Functions for managing memory chunks
+// Functions for managing ByteArr and ValueBuffer
+void set_value(ByteArr arr, ValueBuffer val, int offset, int size) {
+    ValueBuffer mask = 1;
+    for (int i = 0; i < size * BYTE_BITWIDTH; i++) {
+        arr[(i / BYTE_BITWIDTH) + offset] |= (((mask << i) & val) >> ((i / BYTE_BITWIDTH) * BYTE_BITWIDTH));
+    }
+}
+
+ValueBuffer get_value(ByteArr arr, int offset, int size) {
+    ValueBuffer val = 0;
+    for (int i = 0; i < size; i++) {
+        val |= (arr[offset + i] << (BYTE_BITWIDTH * i));
+    }
+    return val;
+}
+
+// Functions for managing MemoryChunk
 MemoryChunk make_memory_chunk(int size, int initial) {
     MemoryChunk result;
     result.size = size;
-    result.body = (int *)malloc(size);
+    result.valid_bitwidth = size * BYTE_BITWIDTH;  // default valid bitwidth = size * 8
+    result.body = (char *)malloc(size);
+    memset(result.body, initial, size);
     return result;
+}
+
+MemoryChunk copy_memory_chunk(MemoryChunk target) {
+    MemoryChunk chunk;
+    chunk.size = target.size;
+    chunk.body = (char *)malloc(target.size);
+    memcpy(chunk.body, target.body, chunk.size);
+    return chunk;
 }
 
 void remove_memory_chunk(MemoryChunk chunk) {
     free(chunk.body);
 }
 
+void remove_compressed_result(CompressionResult result) {
+    remove_memory_chunk(result.compressed);
+}
+
 void print_memory_chunk(MemoryChunk chunk) {
-    for (int i = 0; i < size / sizeof(*chunk); i++) {
-        printf('0x%02x ', chunk.body[i]);
+    printf("size: %dBytes\n", chunk.size);
+
+    int *buffer = (int *)malloc(chunk.size);
+    memcpy(buffer, chunk.body, chunk.size);
+    printf("body: ");
+    for (int i = chunk.size / sizeof(int) - 1; i >= 0; i--) {
+        printf("%08x ", buffer[i]);
     }
+    printf("\n");
+    free(buffer);
+}
+
+void print_memory_chunk_bitwise(MemoryChunk chunk) {
+    Byte buffer, mask;
+
+    printf("valid bitwidth: %dbits", chunk.valid_bitwidth);
+    printf("body: ");
+    for (int i = chunk.valid_bitwidth-1; i >= 0; i--) {
+        buffer = chunk.body[i / BYTE_BITWIDTH];
+        printf("%d", (buffer & mask << (i % BYTE_BITWIDTH)) != 0);
+    }
+    printf("\n");
 }
 
 // Functions for BDI algorithms related functions
-CompressionResult base_plus_delta(CacheLine original);                     // Base + delta algotithm 
-CompressionResult base_delta_immediate(CacheLine original);                // BDI algorithm
-CompressionResult bdi_compressing_unit(CacheLine original, int k, int d);  // Compressing Unit (CU)
+CacheLine base_plus_delta(CacheLine original);                     // Base + delta algotithm 
+CacheLine base_delta_immediate(CacheLine original);                // BDI algorithm
+CacheLine bdi_compressing_unit(CacheLine original, int encoding);  // Compressing Unit (CU)
 
 #endif

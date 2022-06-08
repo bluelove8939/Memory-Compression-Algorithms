066dc7d1842b1a0dec2595e5e2011461c4581bae
Seongwook Kim <bluelove8939@naver.com>
2022-06-08 13:36:33 +0900

    Update testbenches

---
+++ testbench_bdi.c
@@ -0,0 +1,24 @@
+#include "compression.h"
+
+int main(int argc, char const *argv[]) {
+    // Testbench for BDI algorithm
+    CacheLine original = make_memory_chunk(CACHE64SIZ, 0);
+    set_value(original.body, 0x24, 0, 1);
+    set_value(original.body, 0x24, 1, 1);
+    set_value(original.body, 0x24, 2, 1);
+    set_value(original.body, 0x24, 3, 1);
+
+    CompressionResult result = bdi_compression(original);
+    print_compression_result(result);
+    printf("\n");
+
+    DecompressionResult dec_result = bdi_decompression(result.compressed, result.tag_overhead, result.original.size);
+    print_decompression_result(dec_result);
+    printf("\n");
+
+    remove_decompression_result(dec_result);
+    remove_compression_result(result);
+    remove_memory_chunk(original);
+
+    return 0;
+}
--- testbench_fpc.c
+++ testbench_fpc.c
@@ -1,26 +1,6 @@
 #include "compression.h"
 
 int main(int argc, char const *argv[]) {
-    // Testbench for BDI algorithm
-    // CacheLine original = make_memory_chunk(CACHE64SIZ, 0);
-    // set_value(original.body, 0x24, 0, 1);
-    // set_value(original.body, 0x24, 1, 1);
-    // set_value(original.body, 0x24, 2, 1);
-    // set_value(original.body, 0x24, 3, 1);
-
-    // CompressionResult result = bdi_compression(original);
-    // print_compression_result(result);
-    // printf("\n");
-
-    // DecompressionResult dec_result = bdi_decompression(result.compressed, result.tag_overhead, result.original.size);
-    // print_decompression_result(dec_result);
-    // printf("\n");
-
-    // remove_decompression_result(dec_result);
-    // remove_compression_result(result);
-    // remove_memory_chunk(original);
-
-
     // Testbench for FPC algorithm
     CacheLine original = make_memory_chunk(CACHE64SIZ, 0);
     set_value(original.body, 0x00000000, 0,  4);  // prefix 0
@@ -35,6 +15,14 @@ int main(int argc, char const *argv[]) {
     CompressionResult result = fpc_compression(original);
     print_compression_result(result);
     printf("\n");
+
+    DecompressionResult dec_result = fpc_decompression(result.compressed, result.tag_overhead, result.original.size);
+    print_decompression_result(dec_result);
+    printf("\n");
+
+    remove_memory_chunk(original);
+    remove_compression_result(result);
+    remove_decompression_result(dec_result);
 
     return 0;
 }

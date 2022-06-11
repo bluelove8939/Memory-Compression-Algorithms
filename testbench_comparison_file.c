f967078b3a645f6a96b6335a8906120a3e7d083c
Seongwook Kim <bluelove8939@naver.com>
2022-06-12 02:24:37 +0900

    Update testbench

--- testbench_comparison_file.c
+++ testbench_comparison_file.c
@@ -9,8 +9,8 @@ int main(int argc, char const *argv[]) {
     CompressionResult bdi_result, fpc_result;
     char const *filename = "./data/repeating.bin";
     char const *logfilename = "comparison_result.txt";
-    int filesize, bdisize, fpcsize;
-    int chunksize;
+    int filesize, originalsize, bdisize, fpcsize;
+    int chunksize, iter, maxiter = 500;
 
     // freopen(logfilename,"w",stdout);
 
@@ -22,36 +22,49 @@ int main(int argc, char const *argv[]) {
         exit(-1);
     }
 
+    if (argc > 3)
+        maxiter = atoi(argv[3]);
+
     FILE *fp = fopen(filename, "rb");
     fseek(fp, 0, SEEK_END);
     filesize = ftell(fp);
     fclose(fp);
 
+    originalsize = 0;
     bdisize = 0;
     fpcsize = 0;
+    iter = 0;
 
-    for (int i = 0; i < filesize; i += chunksize) {
-#ifdef VERBOSE
-        printf("offset: %dBytes  size: %dBytes\n", i, chunksize);
-#endif
+    for (int i = 0; (i < filesize) && (iter < maxiter); i += chunksize) {
         chunk = file2memorychunk(filename, i, chunksize);
         
         bdi_result = bdi_compression(chunk);
         fpc_result = fpc_compression(chunk);
 
+        originalsize += chunksize;
         bdisize += bdi_result.compressed.size;
         fpcsize += fpc_result.compressed.size;
+
+#ifdef VERBOSE
+        printf("offset: %dBytes  size: %dBytes\n", i, chunksize);
+        printf("original: ");
+        print_memory_chunk(chunk);
+        printf("\n");
+        printf("bdi size: %dBytes  fpc size: %dBytes\n\n", bdi_result.compressed.size, fpc_result.compressed.size);
+#endif
 
         remove_memory_chunk(chunk);
         remove_compression_result(bdi_result);
         remove_compression_result(fpc_result);
+
+        iter += 1;
     }
 
     printf("====================\n");
-    printf("compression ratio: %.4f(BDI) %.4f(FPC)\n", (double)filesize / bdisize, (double)filesize / fpcsize);
-    printf("file size  (%3dBytes)\n", filesize);
-    printf("BDI size   (%3dBytes)\n", bdisize);
-    printf("FPC size   (%3dBytes)\n", fpcsize);
+    printf("compression ratio: %.4f(BDI) %.4f(FPC)\n", (double)originalsize / bdisize, (double)originalsize / fpcsize);
+    printf("original size (%3dBytes)\n", originalsize);
+    printf("BDI size      (%3dBytes)\n", bdisize);
+    printf("FPC size      (%3dBytes)\n", fpcsize);
     printf("====================\n");
 
     return 0;

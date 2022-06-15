0f7d84cb95855f354db05d2f6ab2f63c123695f2
Seongwook Kim <bluelove8939@naver.com>
2022-06-15 10:05:34 +0900

    Add testbench for BDI with Zeros Run algorithm

--- testbench_comparison_file.c
+++ testbench_comparison_file.c
@@ -6,10 +6,10 @@
 
 int main(int argc, char const *argv[]) {
     MemoryChunk chunk;
-    CompressionResult bdi_result, fpc_result, bdi_twobase_result;
+    CompressionResult bdi_result, fpc_result, bdi_twobase_result, bdi_zr_result;
     char const *filename = "./data/repeating.bin";
     char const *logfilename = "comparison_result.txt";
-    int filesize, original_size, bdi_size, fpc_size, bdi_twobase_size;
+    int filesize, original_size, bdi_size, fpc_size, bdi_twobase_size, bdi_zr_size;
     int chunksize, iter, maxiter = 500;
 
     // freopen(logfilename,"w",stdout);
@@ -34,6 +34,7 @@ int main(int argc, char const *argv[]) {
     bdi_size = 0;
     fpc_size = 0;
     bdi_twobase_size = 0;
+    bdi_zr_size = 0;
     iter = 0;
 
     for (int i = 0; (i < filesize) && (iter < maxiter); i += chunksize) {
@@ -42,40 +43,46 @@ int main(int argc, char const *argv[]) {
         bdi_result = bdi_compression(chunk);
         fpc_result = fpc_compression(chunk);
         bdi_twobase_result = bdi_twobase_compression(chunk);
+        bdi_zr_result = bdi_zr_compression(chunk);
 
         original_size += chunksize;
         bdi_size += bdi_result.compressed.size;
         fpc_size += fpc_result.compressed.size;
         bdi_twobase_size += bdi_twobase_result.compressed.size;
+        bdi_zr_size += bdi_zr_result.compressed.size;
 
 #ifdef VERBOSE
         printf("offset: %dBytes  size: %dBytes\n", i, chunksize);
         printf("original: ");
         print_memory_chunk(chunk);
         printf("\n");
-        printf("bdi size: %dBytes  fpc size: %dBytes  bdi twobase size: %dBytes\n\n", 
+        printf("bdi size: %dBytes  fpc size: %dBytes  bdi twobase size: %dBytes  bdi zr size: %dBytes\n\n", 
                 bdi_result.compressed.size, 
                 fpc_result.compressed.size,
-                bdi_twobase_result.compressed.size);
+                bdi_twobase_result.compressed.size,
+                bdi_zr_result.compressed.size);
 #endif
 
         remove_memory_chunk(chunk);
         remove_compression_result(bdi_result);
         remove_compression_result(fpc_result);
         remove_compression_result(bdi_twobase_result);
+        remove_compression_result(bdi_zr_result);
 
         iter += 1;
     }
 
     printf("====================\n");
-    printf("compression ratio: %.4f(BDI) %.4f(FPC) %.4f(BDI 2B)\n", 
+    printf("compression ratio: %.4f(BDI) %.4f(FPC) %.4f(BDI 2B) %.4f(BDI ZR)\n", 
             (double)original_size / bdi_size, 
             (double)original_size / fpc_size, 
-            (double)original_size / bdi_twobase_size);
+            (double)original_size / bdi_twobase_size,
+            (double)original_size / bdi_zr_size);
     printf("original size (%3dBytes)\n", original_size);
     printf("BDI size      (%3dBytes)\n", bdi_size);
     printf("FPC size      (%3dBytes)\n", fpc_size);
     printf("BDI 2B size   (%3dBytes)\n", bdi_twobase_size);
+    printf("BDI ZR size   (%3dBytes)\n", bdi_zr_size);
     printf("====================\n");
 
     return 0;

6188f2ca0c62c5fed3b5d180d9b86adbdfb79615
bluelove8939 <bluelove8939@naver.com>
2022-07-26 14:47:43 +0900

    Update activation image generator

--- tools/activation_imgshow.py
+++ tools/activation_imgshow.py
@@ -1,20 +1,67 @@
-import os
-
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-
-import torchvision
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-
-import numpy as np
 import matplotlib.pyplot as plt
 
-# from tools.progressbar import progressbar
-from tools.training import train, test
-from tools.pruning import PruneModule
+
+AUTO = 'auto'
+DONT_SAVE = 'dont_save'
 
 
+class ActivationImgGenerator(object):
+    def __init__(self, model_savepath, device=AUTO):
+        self.model_savepath = model_savepath
+        self.device = device  # defined target device (mainly 'cpu' or 'cuda')
+        self._activation = {}
+        self._hook_handlers = {}
+
+        if self.device == AUTO:
+            self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    def get_activation_hook(self, name, channel_size):
+        def hook(model, layer_input, layer_output):
+            self._activation[name] = layer_output.detach()[0][:channel_size]
+
+        return hook
+
+    def add_trace(self, layer: torch.nn.Module, name: str, channel_size: int=9):
+        if name not in self._hook_handlers:
+            self._hook_handlers[name] = layer.register_forward_hook(self.get_activation_hook(name, channel_size))
+        else:
+            print(f"Hook '{name}' already exists")
+
+    def remove_trace(self, name: str):
+        self._hook_handlers[name].remove()
+        del self._hook_handlers[name]
+
+    def show_activations(self, test_dataset, model: torch.nn.Module, img_savepath:str=DONT_SAVE):
+        model.load_state_dict(torch.load(self.model_savepath))
+        activation = {}
+
+        data, _ = test_dataset[0]
+        data.unsqueeze_(0)
+        model.eval()
+        model(data.to(self.device))
+
+        rgrid, cgrid = 0, 0
+        for key in activation.keys():
+            rgrid = max(rgrid, activation[key].squeeze().size(0))
+            cgrid += 1
+        print(f"rgrid: {rgrid}, cgrid: {cgrid}")
+
+        fig, axs = plt.subplots(cgrid, rgrid, figsize=(4 * rgrid, 4 * cgrid), gridspec_kw={'width_ratios': [1] * rgrid})
+        fig.suptitle("Intermediate Activation Images")
+
+        ridx, cidx = 0, 0
+        for key in activation.keys():
+            act = activation[key].squeeze()
+            for ridx in range(rgrid):
+                if ridx < act.size(0):
+                    axs[cidx, ridx].imshow(act[ridx].to('cpu'))
+                    axs[cidx, ridx].set_title(f"{key}_channel{ridx}")
+                else:
+                    axs[cidx, ridx].axis('off')
+            cidx += 1
+
+        # plt.tight_layout()
+        plt.show()
+        if img_savepath != DONT_SAVE:
+            plt.savefig(img_savepath)

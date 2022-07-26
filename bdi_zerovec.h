eb06f433020ed5b83de0f7daa3c5f69f241da6af
bluelove8939 <bluelove8939@naver.com>
2022-07-26 17:23:43 +0900

    Update verbose option

--- bdi_zerovec.h
+++ bdi_zerovec.h
@@ -4,7 +4,7 @@
 #include "compression.h"
 
 // Verbose parameter
-#define VERBOSE  // Comment this line not to display debug messages
+// #define VERBOSE  // Comment this line not to display debug messages
 
 CompressionResult bdi_zv_compression(CacheLine original);
 Bool bdi_zv_compressing_unit(CacheLine original, CacheLine *compressed, MetaData *tag_overhead, int encoding);

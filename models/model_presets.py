9fea315aeabde1c04c7d0dbf55987ad8b0a027cb
bluelove8939 <bluelove8939@naver.com>
2022-07-26 16:07:50 +0900

    Update SqueezeNet and InceptionV3

--- models/model_presets.py
+++ models/model_presets.py
@@ -14,4 +14,6 @@ imagenet_pretrained = {
     'ResNet50': ModelConfig(torchvision.models.resnet50, weights=torchvision.models.ResNet50_Weights.IMAGENET1K_V1),
     'AlexNet': ModelConfig(torchvision.models.alexnet, weights=torchvision.models.AlexNet_Weights.IMAGENET1K_V1),
     'VGG16': ModelConfig(torchvision.models.vgg16, weights=torchvision.models.VGG16_Weights.IMAGENET1K_V1),
+    'SqueezeNet': ModelConfig(torchvision.models.squeezenet1_0, weights=torchvision.models.SqueezeNet1_0_Weights.IMAGENET1K_V1),
+    'InceptionV3': ModelConfig(torchvision.models.inception_v3, weights=torchvision.models.Inception_V3_Weights.IMAGENET1K_V1),
 }
